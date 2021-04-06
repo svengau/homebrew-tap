5b31b4721be3d0afda74cbbed9a8be0ee40aacea
Sven Gaubert <svengau@gmail.com>
2021-04-06 07:33:41 +0700

    gogo v0.1.1

--- gogo.rb
+++ gogo.rb
@@ -1,8 +1,8 @@
 class Gogo < Formula
     desc "A tool to inject env vars into cmd"
     homepage "https://github.com/svengau/gogo"
-    url "https://github.com/svengau/gogo/releases/download/v0.1.0/gogo-0.1.0.tar.gz"
-    sha256 "5b5a66efd070519097e60c5d4aff34877cd8f0fe1f7a8ce02df4fe1cac648401"
+    url "https://github.com/svengau/gogo/releases/download/v0.1.1/gogo-0.1.1.tar.gz"
+    sha256 "82afa84d053505549e10f8324ddfd6a41c53a54eeb2367a301453994caed0965"
     
     bottle :unneeded
   
